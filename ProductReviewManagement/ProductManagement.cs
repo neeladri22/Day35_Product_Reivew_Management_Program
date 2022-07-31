142213e8250d31e1cbdb7030b938ce43b7fe76b6
neeladri22 <neeladri22@gmail.com>
2022-07-31 06:18:18 +0530

    Retrive Top 3 Records

---
+++ ProductReviewManagement/ProductManagement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    internal class ProductManagement
+    {
+        //Top 3 Records of High Rating
+        public static void Top3Records(List<ProductReview> Product)
+        {
+            var result = (from PM in Product orderby PM.Rating descending select PM).Take(3);
+            Console.WriteLine("Top 3 Records are :");
+            foreach (ProductReview pm in result)
+            {
+                Console.WriteLine("\nProduct ID: " + pm.ProductId +
+                                    "\nUser ID: " + pm.UserId +
+                                    "\nRating: " + pm.Rating +
+                                    "\nReview: " + pm.Review +
+                                    "\nIS Liked : " + pm.isLike);
+            }
+        }
+    }
+}
