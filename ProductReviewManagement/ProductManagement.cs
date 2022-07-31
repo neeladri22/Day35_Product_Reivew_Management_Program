6b1c92b7ed7f3ef0898366037731d8ced1b45366
neeladri22 <neeladri22@gmail.com>
2022-07-31 06:21:50 +0530

    Retrieve all records who's Rating is Greater that 3 and Product ID is 1 or 4 or 9

--- ProductReviewManagement/ProductManagement.cs
+++ ProductReviewManagement/ProductManagement.cs
@@ -22,5 +22,21 @@ namespace ProductReviewManagement
                                     "\nIS Liked : " + pm.isLike);
             }
         }
+
+        //Retrieve all records who's Rating is Greater that 3 and Product ID is 1 or 4 or 9
+        public static void RetriveAllRecordWhosRatingGreaterThan3(List<ProductReview> Product)
+        {
+            var result = (from PM in Product where (PM.ProductId > 3) && (PM.ProductId == 1 || PM.ProductId == 4 || PM.ProductId == 9) select PM).ToList();
+            Console.WriteLine("\nRetrieve all records who's Rating is Greater than 3 and Product ID is 14 or 4 or 9 :");
+            foreach (ProductReview pm in result)
+            {
+                Console.WriteLine("\nProduct ID: " + pm.ProductId +
+                                    "\nUser ID: " + pm.UserId +
+                                    "\nRating: " + pm.Rating +
+                                    "\nReview: " + pm.Review +
+                                    "\nIS Liked : " + pm.isLike);
+            }
+
+        }
     }
 }
