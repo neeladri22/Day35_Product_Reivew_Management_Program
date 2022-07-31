5ba64bf85339aabd7a2e0eeaaa3e9ce602399a53
neeladri22 <neeladri22@gmail.com>
2022-07-31 06:13:01 +0530

    Creating ProductReview class and adding 25 default values

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace ProductReviewManagement
 {
@@ -7,6 +9,38 @@ namespace ProductReviewManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Management Review system problem");
+
+            //Adding default 25 values
+            List<ProductReview> Product;
+            Product = new List<ProductReview>
+            {
+                new ProductReview() { ProductId = 12, UserId = 13, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 70, UserId = 16, Rating = 2, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 35, UserId = 111, Rating = 1, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 80, UserId = 167, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 16, UserId = 13, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 85, UserId = 15, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 10, UserId = 19, Rating = 1, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 96, UserId = 12, Rating = 2, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 50, UserId = 16, Rating = 3, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 126, UserId = 111, Rating = 2, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 100, UserId = 178, Rating = 1, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 30, UserId = 123, Rating = 3, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 90, UserId = 18, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 20, UserId = 11, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 70, UserId = 16, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 10, UserId = 19, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 90, UserId = 12, Rating = 2, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 40, UserId = 15, Rating = 1, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 60, UserId = 11, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 20, UserId = 15, Rating = 1, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 80, UserId = 19, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 30, UserId = 19, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 50, UserId = 11, Rating = 2, Review = "Good", isLike = false },
+                new ProductReview() { ProductId = 60, UserId = 25, Rating = 3, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 90, UserId = 19, Rating = 1, Review = "Good", isLike = false }
+            };
         }
     }
+    
 }
