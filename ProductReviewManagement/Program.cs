2d74f15068c6d4653477b06b12bdfda1f58a24df
neeladri22 <neeladri22@gmail.com>
2022-07-31 06:07:44 +0530

    Welcome to Product Review Management System problem

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Management Review system problem");
+        }
+    }
+}
